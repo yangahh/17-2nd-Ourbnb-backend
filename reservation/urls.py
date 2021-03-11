c735f188ad34b668eaaa8656738612da907b928f
Gyubster <kyunbo0910@naver.com>
2021-03-11 15:27:15 +0900

    CREATE: PurchaseView

--- reservation/urls.py
+++ reservation/urls.py
@@ -1,6 +1,9 @@
-from django.urls        import path,include
+
+from django.urls        import path
 from reservation.views  import ReservationListView
+from reservation.views  import PurchaseView
 
 urlpatterns = [
-    path('', ReservationListView.as_view())
+    path('', ReservationListView.as_view()),
+    path('/purchase', PurchaseView.as_view())
 ]
